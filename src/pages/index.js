3dbbb7f2c5ec3c8ee62e32335f898da367e9c4dc
kalpshah485 <kalpshah485@gmail.com>
2022-09-05 13:28:07 +0530

    feat: created homepage.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,31 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({ products }) {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
-      {/* Header Section */}
       <Header />
-      <main className="max-v-screen-2xl mx-auto">
-        {/* banner */}
-
-        
-
+      <main className="max-w-screen-2xl mx-auto">
+        <Banner />
+        <ProductFeed products={products} />
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const products = await fetch('https://fakestoreapi.com/products').then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      products
+    }
+  }
 }

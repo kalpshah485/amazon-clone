e3a26827f969d2ae2157f32573c7c0418af6a4d8
kalpshah485 <kalpshah485@gmail.com>
2022-09-12 10:57:04 +0530

    added localstorage implementation with redux.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,8 +2,18 @@ import { Provider } from 'react-redux';
 import { store } from '../app/store';
 import '../styles/globals.css';
 import { Provider as AuthProvider } from 'next-auth/client';
+import { useEffect } from 'react';
+import { useSetDataToBasket } from "../assets/utils";
 
 const MyApp = ({ Component, pageProps }) => {
+
+  const dispatch = store.dispatch;
+
+  useEffect(() => {
+    const localStorageItems = localStorage.getItem('basket') ? JSON.parse(localStorage.getItem('basket')) : [];
+    useSetDataToBasket(dispatch, localStorageItems)
+  }, [])
+
   return (
     <AuthProvider session={pageProps.session}>
       <Provider store={store}>
